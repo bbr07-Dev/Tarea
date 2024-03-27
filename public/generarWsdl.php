6f620daae4dd2c958902ffc31902787d13891630
bbr07-Dev <barbaragestion@gmail.com>
2024-03-27 09:53:22 +0100

    servicio.wsdl generado

--- public/generarWsdl.php
+++ public/generarWsdl.php
@@ -1,10 +1,10 @@
 <?php
-
     require '../vendor/autoload.php';
+    require '../src/Operaciones.php';
 
     use PHP2WSDL\PHPClass2WSDL;
-    $class = "barbara\\tarea\\src\\Operaciones";
-    $uri = 'http://localhost:3000/servidorSoap/serviciorW.php';
+    $class = 'barbara\tarea\src\Operaciones';
+    $uri = 'http://localhost:3000/servidorSoap/servicioW.php';
     $wsdlGenerator = new PHPClass2WSDL($class, $uri);
     $wsdlGenerator->generateWSDL(true);
     $fichero = $wsdlGenerator-> save('../servidorSoap/servicio.wsdl');
